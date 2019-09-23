2880aa63eaf5d96b13d923a54ebd0f6601b64d84
c4710n <c4710n@users.noreply.github.com>
2019-09-23 18:05:02 +0800

    init: initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const { name } = require('./package.json')
+const path = require('path')
+const os = require('os')
+const { spawn } = require('child_process')
+const fs = require('fs-extra')
+const localtime = require('localtime')
+
+const TEMPLATE_DIR = path.join(__dirname, 'template')
+const BUILD_BASE_DIR = path.join('/tmp', `${name}-build`)
+
+function mipi(inputFile, outputFile, { logoFile, mediaPath } = {}) {
+  cloneDir(TEMPLATE_DIR, BUILD_BASE_DIR)
+  copyContent(inputFile, BUILD_BASE_DIR)
+
+  const locals = generateLocals({
+    logo: logoFile,
+    updated_at: localtime('', 'YYYY-MM-DD hh:mm'),
+  })
+
+  spawn(
+    'npx',
+    ['relaxed', 'index.pug', outputFile, '--build-once', '--locals', locals],
+    {
+      cwd: BUILD_BASE_DIR,
+      detached: true,
+      stdio: 'inherit',
+    }
+  )
+}
+
+module.exports = mipi
+
+function cloneTemplate(destPath) {
+  fs.ensureDirSync(destPath)
+  fs.copySync(TEMPLATE, destPath)
+}
+
+function cloneDir(srcPath, destPath) {
+  fs.ensureDirSync(destPath)
+  fs.copySync(srcPath, destPath)
+}
+
+function copyContent(filePath, dirPath) {
+  const srcFile = filePath
+  const destFile = path.join(dirPath, 'content.md')
+
+  fs.copySync(srcFile, destFile)
+}
+
+function generateLocals({ logo, updated_at } = {}) {
+  const raw = {}
+
+  if (logo) {
+    raw.logo = logo
+  }
+
+  if (updated_at) {
+    raw.updated_at = updated_at
+  }
+
+  return JSON.stringify(raw)
+}
