7d72f8f054dbe3bd23bc28f04881f0f550d689d5
m31271n <m31271n@users.noreply.github.com>
2019-09-23 18:05:02 +0800

    init: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# mipi
+
+> A opinionated tool that aim to convert Markdown to PDF.
+
+> Based on [ReLaXed](https://github.com/RelaxedJS).
+
+## Installation
+
+```sh
+npm install mipi
+```
+
+## Usage
+
+```sh
+mipi --help
+```
+
+## License
+
+[MIT](https://2players.studio/licenses/MIT) © [2Players Studio](https://2players.studio/)
---
+++ bin/cli.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+'use strict'
+
+const { name } = require('../package.json')
+const mipi = require('../index')
+const path = require('path')
+const yargs = require('yargs')
+
+const { argv } = yargs
+  .scriptName(name)
+  .usage('$0 <input> <output> [options]')
+  .option('l', {
+    alias: 'logo',
+    demandOption: false,
+    describe: 'specify logo file',
+    type: 'string',
+  })
+  .option('m', {
+    alias: 'media',
+    demandOption: false,
+    describe: 'specify directory for placing medias',
+    type: 'string',
+  })
+  .help()
+
+const {
+  _: [input, output],
+  logo,
+  media,
+} = argv
+
+if (!input) {
+  console.error('input must be provided.\n')
+  yargs.showHelp()
+  return
+}
+
+if (!output) {
+  console.error('output must be provied.\n')
+  yargs.showHelp()
+  return
+}
+
+const cwd = process.cwd()
+const inputFile = path.join(cwd, input)
+const outputFile = path.join(cwd, output)
+
+const options = {}
+if (logo) {
+  const logoFile = path.join(cwd, logo)
+  options.logoFile = logoFile
+}
+
+if (media) {
+  const mediaPath = path.join(cwd, mediaPath)
+  options.mediaPath = mediaPath
+}
+
+mipi(inputFile, outputFile, options)
